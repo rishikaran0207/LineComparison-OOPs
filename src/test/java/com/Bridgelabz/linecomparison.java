c4e14730c33b3d1fb644a16a5400a0434916fe93
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-03 22:05:07 +0530

    Added: Welcome

---
+++ src/test/java/com/Bridgelabz/linecomparison.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz;
+
+public class linecomparison {
+
+
+    public static void main(String[] args) {
+        System.out.println("\n Welcome to Line Comparison Computation Program");
+
+    }
+}
