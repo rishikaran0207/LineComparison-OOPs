68f7badcfb1e5bc2026b318b406014d2f9bf8ea9
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-03 22:25:20 +0530

    Added: Line is Equal

--- src/test/java/com/Bridgelabz/linecomparison.java
+++ src/test/java/com/Bridgelabz/linecomparison.java
@@ -5,6 +5,24 @@ public class linecomparison {
 
     Scanner sc=new Scanner(System.in);
     public static int x1,x2,y1,y2;
+    public static double length,flength,slength;
+
+    public void getvalue()
+    {
+        System.out.println("\n\ngetting two line input:");
+        System.out.println("\n  First line:");
+        values();
+        System.out.println("\n first Line");
+        length();
+        flength=length;
+        System.out.print("\n  The first line for the values ("+x1+","+x2+") , (y1,y2) as ("+y1+","+y2+") is "+flength);
+        System.out.println("\n  for second line:");
+        values();
+        System.out.println("\n\nFor second line");
+        length();
+        slength=length;
+        System.out.print("\n  The second line for the values of (x1,x2) as ("+x1+","+x2+") & (y1,y2) as ("+y1+","+y2+") is "+slength);
+    }
 
     public void values()
     {
@@ -25,12 +43,24 @@ public class linecomparison {
         System.out.println("\n  The length ("+x1+","+x2+"),("+y1+","+y2+") is "+length);
     }
 
+    public void checkequality()
+    {
+        System.out.println("\n Equality of line:");
+        Double line1=new Double(flength);
+        Double line2=new Double(slength);
+        boolean equal= line1.equals(line2);
+        if(equal)
+            System.out.println("Boundary of 1st line is equal to endpoint of 2nd line");
+        else
+            System.out.println("Boundary of 1st line is not equal to endpoint of 2nd line");
+    }
+
     public static void main(String[] args) {
 
         System.out.println("\n Welcome to Line Comparison Computation Program");
 
         linecomparison line= new linecomparison();
-        line.values();
-        line.length();
+        line.getvalue();
+        line.checkequality();
     }
 }
